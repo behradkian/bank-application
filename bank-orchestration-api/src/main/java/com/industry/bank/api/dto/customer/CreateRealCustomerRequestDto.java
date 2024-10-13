ecaea691818e6a8772d1622940e59882c3a9e61a
Pedram Behradkian <behradkian@outlook.com>
2024-10-13 23:12:39 +0330

    [feature] add swagger schema in dtos

--- bank-orchestration-api/src/main/java/com/industry/bank/api/dto/customer/CreateRealCustomerRequestDto.java
+++ bank-orchestration-api/src/main/java/com/industry/bank/api/dto/customer/CreateRealCustomerRequestDto.java
@@ -1,7 +1,11 @@
 package com.industry.bank.api.dto.customer;
 
+import com.industry.bank.api.dto.file.ImageDto;
+import com.industry.bank.api.dto.general.NationalityDto;
+import com.industry.bank.api.dto.location.AddressDto;
 import com.industry.bank.api.dto.location.CityDto;
 import com.industry.bank.api.enumeration.customer.GenderType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,20 +16,28 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CreateRealCustomerRequestDto extends CreateGeneralCustomerRequestDto{
+@Schema(name = "CreateRealCustomerRequestDto", title = "", description = "")
+public class CreateRealCustomerRequestDto implements GeneralCustomerRequestDto{
 
-    private String passportNumber;
-    private String nationalCode;
+
     private String firstName;
     private String lastName;
+    private String fatherName;
     private String mobileNumber;
+    private String passportNumber;
+    private String nationalCode;
+    private NationalityDto nationality;
 
     private Date birthdate;
     private CityDto birthCity;
 
+    private String phoneNumber;
+    private String email;
     private GenderType gender;
-
     private OccupationDto occupation;
+
+    private List<AddressDto> addresses;
+    private List<ImageDto> images;
     private List<RelatedCustomerDto> customerRelations;
 
 }
