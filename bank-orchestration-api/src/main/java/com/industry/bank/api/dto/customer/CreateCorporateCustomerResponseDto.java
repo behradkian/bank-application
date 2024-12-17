b3bbcd2815e589e206518292bcec845df20f42a8
Pedram Behradkian <behradkian@outlook.com>
2024-12-17 19:29:17 +0330

    [enhancement] modify documents

--- bank-orchestration-api/src/main/java/com/industry/bank/api/dto/customer/CreateCorporateCustomerResponseDto.java
+++ bank-orchestration-api/src/main/java/com/industry/bank/api/dto/customer/CreateCorporateCustomerResponseDto.java
@@ -3,16 +3,20 @@ package com.industry.bank.api.dto.customer;
 import com.industry.bank.api.enumeration.customer.CustomerType;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-@Getter
+@EqualsAndHashCode(callSuper = true)
+@Data
 @AllArgsConstructor
 @Schema(name = "CreateCorporateCustomerResponseDto", title = "", description = "")
-public class CreateCorporateCustomerResponseDto extends CreateGeneralCustomerResponseDto{
+public class CreateCorporateCustomerResponseDto extends CreateGeneralCustomerResponseDto {
 
     @Hidden
     @Builder.Default
-    @Schema(title = "", name = "type", description = "", defaultValue = "CORPORATE")
+    @Schema(name = "type", title = "", description = "", defaultValue = "CORPORATE")
     private CustomerType type = CustomerType.CORPORATE;
 
 }
--- bank-orchestration-api/src/main/java/com/industry/bank/api/dto/customer/CreateRealCustomerResponseDto.java
+++ bank-orchestration-api/src/main/java/com/industry/bank/api/dto/customer/CreateRealCustomerResponseDto.java
@@ -5,16 +5,19 @@ import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-@Getter
+
+@EqualsAndHashCode(callSuper = true)
+@Data
 @AllArgsConstructor
 @Schema(name = "CreateRealCustomerResponseDto", title = "", description = "")
-public class CreateRealCustomerResponseDto extends CreateGeneralCustomerResponseDto{
+public class CreateRealCustomerResponseDto extends CreateGeneralCustomerResponseDto {
 
     @Hidden
     @Builder.Default
-    @Schema(title = "", name = "type", description = "", defaultValue = "REAL")
+    @Schema(name = "type", title = "", description = "", defaultValue = "REAL")
     private CustomerType type = CustomerType.REAL;
 
 }
