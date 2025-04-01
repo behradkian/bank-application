b0d9b887a2a2583b9da9ed317903e86b10cf74d9
Pedram Behradkian <behradkian@outlook.com>
2025-04-02 00:15:04 +0330

    [feature] customer

--- bank-orchestration-api/src/main/java/com/industry/bank/api/dto/location/CountryDto.java
+++ bank-orchestration-api/src/main/java/com/industry/bank/api/dto/location/CountryDto.java
@@ -1,5 +1,6 @@
 package com.industry.bank.api.dto.location;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +8,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(name = "CountryDto")
 public class CountryDto {
 
+    @Schema(name = "code")
     private String code;
+
+    @Schema(name = "name")
     private String name;
+
 }
